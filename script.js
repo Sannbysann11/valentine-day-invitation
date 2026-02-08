fdea82fb45d5a0eb39f555050c151bad340d13ff
Sannbysann11 <bacpt@futurevn.net>
2026-02-08 23:12:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let leftPos = 0
 let bottomPos = 0
 document.getElementById('yessir').addEventListener('click', () => {
-    alert('Congratulations! You have been invited to a Valentine\'s Day dinner. Please RSVP by February 10th. We hope to see you there!')
+    alert('Cảm ơn em vì đã đồng ý làm lành với anh, anh yêu em rất nhiều. Cho anh mời em đi ăn valentine vào tối thứ 13/2 nhé. Anh đã chuẩn bị 3 nhà hàng ưng ý rùi, em nhắn anh để chọn ra nhà hàng em yêu thích nhé <3 . Mãi yêu em <3')
 })
 document.getElementById('nooo').addEventListener('mouseover', () => {
     leftPos = Math.floor(Math.random() * (50 - (-50) + 1)) + (-50);
